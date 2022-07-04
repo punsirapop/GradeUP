811dadf864e0da78a4b20b2f6a972212357de87d
PatcharaP <po.patchara_st@tni.ac.th>
2022-07-04 11:48:25 +0700

    Add HP System

---
+++ Assets/Script/HealthSystem.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthSystem : MonoBehaviour
+{
+    protected float max_HP, Current_HP;
+
+    protected void SetMaxHP(float _max_HP)
+    {
+        this.max_HP = _max_HP;
+    }
+
+    protected void GetDamage(float _damage)
+    {
+        if (Current_HP - _damage > 0)
+        {
+            this.Current_HP -= _damage;
+        }
+        else
+        {
+            Debug.Log("You died");
+        }
+    }
+    protected void GetHeal(float amount)
+    {
+        this.Current_HP += amount;
+    }
+    protected IEnumerator GetOvertimeDamage(float _damage, int time)
+    {
+        for (int i = 1; i <= time; i++)
+        {
+            GetDamage(_damage);
+            yield return new WaitForSeconds(1f);
+        }
+    }
+}
--- Assets/Script/ItemScript.cs
+++ Assets/Script/ItemScript.cs
@@ -6,18 +6,15 @@ public class ItemScript : MonoBehaviour
 {
     [SerializeField] itemSO ItemStat;
     public float HP,Atk, Atk_Speed, MoveSpeed, Art;
+
+    public delegate void AddStatDelegate(int hp, int atk, int atk_speed, int speed);
+    public AddStatDelegate AddStat;
     void Start()
     {
         this.HP = ItemStat.HP;
         this.Atk = ItemStat.Attack;
         this.Atk_Speed = ItemStat.AttackSpeed;
         this.MoveSpeed = ItemStat.Speed;
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
